229aba5ed19f740ca3e4af02977e6eece991dfab
Jonathan Ramirez <jonny_0385@hotmail.com>
2020-10-01 15:05:59 -0300

    integracion MP

--- notification.php
+++ notification.php
@@ -4,7 +4,7 @@
  
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
     
-    error_log("Error message\n", 3, "captura.log");
+    file_put_contents('captura.log', print_r('mensaje', true), FILE_APPEND);
     
     switch($_POST["type"]) {
         case "payment":
