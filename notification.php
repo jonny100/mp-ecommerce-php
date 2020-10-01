dd4987262acb53407fbc213b605b7ad13ceefafa
Jonathan Ramirez <jonny_0385@hotmail.com>
2020-10-01 14:54:32 -0300

    integracion MP

--- notification.php
+++ notification.php
@@ -4,12 +4,12 @@
  
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
     
-    $file = fopen("captura.log", "w");
-    fwrite($file, "inicio" . PHP_EOL);
-    fwrite($file, json_encode($_POST) . PHP_EOL);
-    fwrite($file, "fin" . PHP_EOL);
-    fclose($file);
-
+//    $file = fopen("captura.log", "w");
+//    fwrite($file, "inicio" . PHP_EOL);
+//    fwrite($file, json_encode($_POST) . PHP_EOL);
+//    fwrite($file, "fin" . PHP_EOL);
+//    fclose($file);
+    
     switch($_POST["type"]) {
         case "payment":
             $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
@@ -26,17 +26,23 @@
     }
     
     // set the status
-    $status_header = 'HTTP/1.1 200 OK';
-    header($status_header);
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
-    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-    // and the content type
-    header('Content-type: application/json');
-    
-    $data = array('mensaje' => 'OK.');
-    echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-    exit;
+//    $status_header = 'HTTP/1.1 200 OK';
+//    header($status_header);
+//    header("Access-Control-Allow-Origin: *");
+//    header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+//    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+//    // and the content type
+//    header('Content-type: application/json');
+//    
+//    $data = array('mensaje' => 'OK.');
+//    echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    //exit;
 ?>
 
-<script src="https://www.mercadopago.com/v2/security.js"></script>
+
+<html> 
+    <script src="https://www.mercadopago.com/v2/security.js"></script>
+    <script>
+    console.log("Database connected!")
+    </script>
+</html>
