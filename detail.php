3f8b7eb173a55a4fd4f1a936cf99ef7002cd0533
Jonathan Ramirez <jonny_0385@hotmail.com>
2020-10-01 11:14:21 -0300

    integracion MP

--- detail.php
+++ detail.php
@@ -6,6 +6,8 @@
 
     // Agrega credenciales
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+    
+    MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
     // Crea un objeto de preferencia
     $preference = new MercadoPago\Preference();
@@ -194,7 +196,7 @@
                                             <?php echo "Cantidad: " . $_POST['unit'] ?>
                                         </h3>
                                     </div>
-                                    <form action="/procesar-pago" method="POST">
+                                    <form action="/procesar-pago.php" method="POST">
                                         <script
                                          src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
                                          data-button-label="Pagar la compra"
